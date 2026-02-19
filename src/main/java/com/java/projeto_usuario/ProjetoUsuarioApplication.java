0794d0899d10debe57910d87dd2f48c0af47ae05
lpr1596@gmail.com <lpr@MacBook-Air-de-Lucas.local>
2026-02-19 15:05:30 -0300

    commit incial

---
+++ src/main/java/com/java/projeto_usuario/ProjetoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.java.projeto_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/projeto_usuario/ProjetoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.projeto_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
